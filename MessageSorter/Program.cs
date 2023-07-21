86af9e5fd5bccffc14c24473545c6ce6c22d453f
Astridalia <79605349+Astridalia@users.noreply.github.com>
2023-07-21 21:36:16 +0000

    Update Program.cs

--- MessageSorter/Program.cs
+++ MessageSorter/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 using System.Text.RegularExpressions;
-using Microsoft.VisualBasic;
 
 namespace MessageOrganiser
 {
@@ -13,61 +11,70 @@ namespace MessageOrganiser
     {
         static void Main(string[] args)
         {
-            string filetoopen = System.IO.Path.GetFileName(@args[0]);
-            string currentfolder = System.IO.Path.GetDirectoryName(@args[0]);
-            string Messages = File.ReadAllText(filetoopen);
-
-            int startPos = Messages.IndexOf(@"<ServiceID TYPE=""UBYT"">") + @"<ServiceID TYPE=""UBYT"">".Length;
-            int length = Messages.IndexOf("</ServiceID>") - startPos;
-            string svcid = Messages.Substring(startPos, length);
-
-            startPos = Messages.IndexOf(@"<ProtocolType TYPE=""STR"">") + @"<ProtocolType TYPE=""STR"">".Length;
-            length = Messages.IndexOf("</ProtocolType>") - startPos;
-            string svcname = Messages.Substring(startPos, length);
-
-            var RECORD_tag_indicies = Regex.Matches(Messages, "<RECORD>");
-            var RECORD_CLOSE_tag_indicies = Regex.Matches(Messages, "</RECORD>");
-            var CLOSE_PROTOCOL_INFO = Regex.Match(Messages, "</_ProtocolInfo>");
-            
-            var aStringBuilder = new StringBuilder(Messages);
-            for (var i = RECORD_CLOSE_tag_indicies.Count-1; i > 0; i--)
+            if (args.Length == 0)
             {
-                var a = RECORD_tag_indicies[i].Index;
-                var b = RECORD_CLOSE_tag_indicies[i].Index + "</RECORD>".Length;
-                aStringBuilder.Remove(a, b - a);
-            }
-            aStringBuilder.Remove(0, CLOSE_PROTOCOL_INFO.Index + "</_ProtocolInfo>".Length);
-            Messages = aStringBuilder.ToString();
-
-            var m_lines = Messages.Split(new char[] { '\n' });
-            string[] filtered_lines = m_lines.Where(s => !s.Trim().StartsWith("</")).ToArray();
-            filtered_lines = filtered_lines.Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
-
-            for (var j = 0; j < filtered_lines.Length; j++)
-            {
-                var t = filtered_lines[j].Trim();
-                filtered_lines[j] = t.Substring(1, t.Length - 2);
+                Console.WriteLine("Please provide a file path as a command-line argument.");
+                return;
             }
 
-            //Array.Sort(filtered_lines, (x, y) => String.CompareOrdinal(x, y));
+            string filePath = args[0];
+            string fileName = Path.GetFileName(filePath);
+            string currentFolder = Path.GetDirectoryName(filePath);
 
-            var res_list = filtered_lines.Distinct().ToList();
-            res_list.Sort((Comparison<String>)(
-            (String left, String right) => 
+            string messages = File.ReadAllText(filePath);
+
+            string serviceId = ExtractTagContent(messages, "<ServiceID TYPE=\"UBYT\">", "</ServiceID>");
+            string serviceName = ExtractTagContent(messages, "<ProtocolType TYPE=\"STR\">", "</ProtocolType>");
+
+            string cleanedMessages = CleanMessages(messages);
+
+            string[] filteredLines = cleanedMessages
+                .Split(new char[] { '\n' })
+                .Where(line => !line.Trim().StartsWith("</") && !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Trim().Substring(1, line.Trim().Length - 2))
+                .Distinct()
+                .OrderBy(line => line, StringComparer.Ordinal)
+                .ToArray();
+
+            string outputFile = Path.Combine(currentFolder, $"{serviceId}_{serviceName}.txt");
+            WriteOutputToFile(outputFile, filteredLines);
+        }
+
+        static string ExtractTagContent(string input, string startTag, string endTag)
+        {
+            int startPos = input.IndexOf(startTag) + startTag.Length;
+            int length = input.IndexOf(endTag) - startPos;
+            return input.Substring(startPos, length);
+        }
+
+        static string CleanMessages(string messages)
+        {
+            int startProtocolInfo = messages.IndexOf("</_ProtocolInfo>") + "</_ProtocolInfo>".Length;
+            string cleanedMessages = messages.Substring(startProtocolInfo);
+
+            var recordTagIndices = Regex.Matches(cleanedMessages, "<RECORD>");
+            var recordCloseTagIndices = Regex.Matches(cleanedMessages, "</RECORD>");
+
+            var stringBuilder = new StringBuilder(cleanedMessages);
+            for (var i = recordCloseTagIndices.Count - 1; i >= 0; i--)
             {
-                return String.CompareOrdinal(left, right);
+                var a = recordTagIndices[i].Index;
+                var b = recordCloseTagIndices[i].Index + "</RECORD>".Length;
+                stringBuilder.Remove(a, b - a);
             }
-            ));
 
-            string filename = svcid + "_" + svcname + ".txt";
-            string output = filename;
-            
-            for(int i = 0; i< res_list.Count; i++)
+            return stringBuilder.ToString();
+        }
+
+        static void WriteOutputToFile(string filePath, string[] lines)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
             {
-                output = output + "\n" + (i+1) + ": " + res_list[i];
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    writer.WriteLine($"{i + 1}: {lines[i]}");
+                }
             }
-
-            File.WriteAllText(currentfolder + "\\" + filename, output);
         }
     }
 }
