3ae4e609af5b78ff4403d6cbaac008ec9b9bc7a1
Astridalia <79605349+Astridalia@users.noreply.github.com>
2023-07-21 21:42:20 +0000

    Update Program.cs

--- Open101-master/Open101.Tools.CaptureAnalyser/Program.cs
+++ Open101-master/Open101.Tools.CaptureAnalyser/Program.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using DragonLib.IO;
-using Haukcode.PcapngUtils.Common;
 using Haukcode.PcapngUtils.PcapNG;
-using Open101.IO;
 using Open101.Net;
 using Open101.Serializer.DML;
-using Open101.Serializer.PropertyClass;
 using PacketDotNet;
 using SerializerPlayground;
 
@@ -16,13 +11,11 @@ namespace Open101.Tools.CaptureAnalyser
 {
     public static class Program
     {
-        public static AnalysisHandlerBase s_handler;
+        private static readonly AnalysisHandlerBase s_handler = new AnalysisHandlerBase();
 
         public static void Main(string[] args)
         {
             SerializerPlayground.Program.Init();
-
-            s_handler = new ObjectAnalysisHandler();
 
             using var reader = new PcapNGReader(@"D:\re\wiz101\net\ravenwood.pcapng", false);
             CancellationTokenSource source = new CancellationTokenSource();
@@ -63,7 +56,8 @@ namespace Open101.Tools.CaptureAnalyser
                 SetLoginActive();
                 m_loginInCapture.ConsumeBuffer(packet.PayloadData, packet.PayloadData.Length);
                 handled = true;
-            } else if (packet.DestinationPort == 12000)
+            }
+            else if (packet.DestinationPort == 12000)
             {
                 if (packet.PayloadData.Length == 0) return;
                 SetLoginActive();
@@ -75,7 +69,8 @@ namespace Open101.Tools.CaptureAnalyser
             {
                 m_zoneInCapture.ProcessPacket(packet);
                 handled = true;
-            } else if (packet.DestinationPort == m_currentZonePort)
+            }
+            else if (packet.DestinationPort == m_currentZonePort)
             {
                 m_zoneOutCapture.ProcessPacket(packet);
                 handled = true;
@@ -83,11 +78,11 @@ namespace Open101.Tools.CaptureAnalyser
 
             if (handled && packet.PayloadData.Length > 0)
             {
-                Logger.Debug("Packet", DateTimeOffset.FromUnixTimeSeconds((long) timestamp).ToString());
+                Logger.Debug("Packet", DateTimeOffset.FromUnixTimeSeconds((long)timestamp).ToString());
             }
         }
 
-        public void SetLoginActive()
+        private void SetLoginActive()
         {
             if (m_loginActive) return;
             m_currentZonePort = 0;
@@ -95,14 +90,13 @@ namespace Open101.Tools.CaptureAnalyser
             m_zoneInCapture = null;
             m_zoneOutCapture = null;
 
-
             m_loginInCapture = CreatePacketHandler();
             m_loginOutHandler = CreatePacketHandler();
             m_loginInCapture.m_displayName = "Login In";
             m_loginOutHandler.m_displayName = "Login Out";
         }
 
-        public void SetZoneActive()
+        private void SetZoneActive()
         {
             m_loginActive = false;
             m_loginInCapture = null;
@@ -151,34 +145,6 @@ namespace Open101.Tools.CaptureAnalyser
 
         public bool NetHandleCONNECTIONSTATS(WIZARD2_53_Protocol.MSG_CONNECTIONSTATS msg)
         {
-            return true;
-        }
-    }
-
-    public class ObjectAnalysisHandler : AnalysisHandlerBase, GAME_5_Protocol.Handler
-    {
-        public int m_idx;
-        
-        public bool NetHandleNewObject(GAME_5_Protocol.MSG_NEWOBJECT msg)
-        {
-            //File.WriteAllBytes($"objects\\object{m_idx}", msg.m_data.GetBytes());
-
-            using var byteBuffer = new ByteBuffer(msg.m_data.GetBytes());
-            var clsId = byteBuffer.ReadUInt8();
-            var unk = byteBuffer.ReadUInt8();
-            var templateId = byteBuffer.ReadUInt32();
-
-            var location = SerializerPlayground.Program.s_templateManifest.GetLocation(templateId);
-            Logger.Success($"Object:{m_idx}", $"{location.m_filename}");
-
-            if (m_idx == 26)
-            {
-                byteBuffer.GetCurrentStream().Position = 0;
-                var inst = new SerializerCoreDataBinaryInstance(byteBuffer, 0x1C);
-                var propClass = (ClientObject)SerializerBinary.ReadObject(inst);
-            }
-            
-            m_idx++;
             return true;
         }
     }
