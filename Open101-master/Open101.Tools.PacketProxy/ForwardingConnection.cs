5a16b7ccb12b205bfa5be9ee27c6c380f06d35b1
Astridalia <79605349+Astridalia@users.noreply.github.com>
2023-07-21 21:37:50 +0000

    Update ForwardingConnection.cs

--- Open101-master/Open101.Tools.PacketProxy/ForwardingConnection.cs
+++ Open101-master/Open101.Tools.PacketProxy/ForwardingConnection.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using Open101.Net;
-using Open101.Serializer.DML;
 using SerializerPlayground;
 
 namespace Open101.Tools.PacketProxy
@@ -13,87 +12,84 @@ namespace Open101.Tools.PacketProxy
     public class ForwardingConnection : KISocket
     {
         protected static int s_currConnectionID;
-        
-        public ForwardingConnection m_peer;
-        public PacketDump m_dump;
 
-        public ProxyServer m_proxy;
-	    
-        public ForwardingConnection(Socket socket) : base(socket) {}
-	    
+        public ForwardingConnection PeerConnection { get; set; }
+        public PacketDump Dump { get; set; }
+        public ProxyServer Proxy { get; set; }
+
+        public ForwardingConnection(Socket socket) : base(socket) { }
+
         public override void Start()
         {
             m_handler = new ProxyKIPacketHandler(this);
         }
-	    
+
         public override void Update()
         {
             if (IsClosed())
             {
-                if (!m_peer.IsClosed())
+                if (!PeerConnection.IsClosed())
                 {
-                    m_peer.Close();
+                    PeerConnection.Close();
                 }
 
-                m_peer.OnSocketClosed();
-            } else if (m_peer.IsClosed())
+                PeerConnection.OnSocketClosed();
+            }
+            else if (PeerConnection.IsClosed())
             {
-                m_peer.OnSocketClosed();
+                PeerConnection.OnSocketClosed();
                 Close();
             }
         }
-	    
+
         public override void ReadHandler(SocketAsyncEventArgs args)
         {
             base.ReadHandler(args);
-            
-            m_dump.Write(args.Buffer, args.BytesTransferred);
 
-            var proxyHandler = (ProxyKIPacketHandler) m_handler;
+            Dump.Write(args.Buffer, args.BytesTransferred);
+
+            var proxyHandler = (ProxyKIPacketHandler)m_handler;
             if (proxyHandler.m_pendingSend.Count > 0)
             {
-                m_peer.Send(proxyHandler.m_pendingSend.ToArray());
+                PeerConnection.Send(proxyHandler.m_pendingSend.ToArray());
                 proxyHandler.m_pendingSend.Clear();
             }
         }
 
-        public static readonly Random s_random = new Random();
+        public static readonly Random Random = new Random();
 
         public override void OnSocketClosed()
         {
-            if (m_dump != null)
-            {
-                m_dump.Dispose();
-                m_dump = null;
-            }
+            Dump?.Dispose();
+            Dump = null;
         }
     }
 
     public class ProxyKIPacketHandler : LoggingKIPacketHandler
     {
-        public ForwardingConnection m_connection;
-        public List<INetworkMessage> m_pendingSend;
+        public ForwardingConnection Connection { get; }
+        public List<INetworkMessage> PendingSend { get; }
 
         public ProxyKIPacketHandler(ForwardingConnection connection)
         {
-            m_connection = connection;
-            m_pendingSend = new List<INetworkMessage>();
+            Connection = connection;
+            PendingSend = new List<INetworkMessage>();
 
-            m_displayName = m_connection.GetRemoteEndPoint().ToString();
+            m_displayName = Connection.GetRemoteEndPoint().ToString();
         }
-        
+
         protected override void HandleMessage(INetworkMessage message, INetworkService service)
         {
             base.HandleMessage(message, service);
-            
+
             if (message is LOGIN_7_Protocol.MSG_CHARACTERSELECTED characterSelected)
             {
                 if (characterSelected.m_prepPhase != 0 && characterSelected.m_IP == string.Empty) return;
-			    
+
                 var proxy = CreateZoneProxy(characterSelected.m_IP, characterSelected.m_TCPPort);
-			    
-                characterSelected.m_IP = proxy.m_localEndpoint.Address.ToString();
-                characterSelected.m_TCPPort = proxy.m_localEndpoint.Port;
+
+                characterSelected.m_IP = proxy?.LocalEndpoint.Address.ToString();
+                characterSelected.m_TCPPort = proxy?.LocalEndpoint.Port ?? 0;
             }
 
             if (message is GAME_5_Protocol.MSG_SERVERTRANSFER transfer)
@@ -101,52 +97,51 @@ namespace Open101.Tools.PacketProxy
                 var mainProxy = CreateZoneProxy(transfer.m_IP, transfer.m_TCPPort);
                 var fallbackProxy = CreateZoneProxy(transfer.m_fallbackIP, transfer.m_fallbackTCPPort);
 
-                transfer.m_IP = mainProxy.m_localEndpoint.Address.ToString();
-                transfer.m_TCPPort = mainProxy.m_localEndpoint.Port;
+                transfer.m_IP = mainProxy?.LocalEndpoint.Address.ToString();
+                transfer.m_TCPPort = mainProxy?.LocalEndpoint.Port ?? 0;
 
                 if (fallbackProxy != null)
                 {
-                    transfer.m_fallbackIP = fallbackProxy.m_localEndpoint.Address.ToString();
-                    transfer.m_fallbackTCPPort = fallbackProxy.m_localEndpoint.Port;
+                    transfer.m_fallbackIP = fallbackProxy.LocalEndpoint.Address.ToString();
+                    transfer.m_fallbackTCPPort = fallbackProxy.LocalEndpoint.Port;
                 }
             }
 
             if (message is WIZARD2_53_Protocol.MSG_CONNECTIONSTATS stats)
             {
                 var proxy = Program.GetProxyServer(stats.m_serverPort);
-                stats.m_serverHostname = proxy.m_hostname;
-                stats.m_serverPort = proxy.m_hostPost;
+                stats.m_serverHostname = proxy?.Hostname;
+                stats.m_serverPort = proxy?.HostPost ?? 0;
             }
-            
-            m_pendingSend.Add(message);
+
+            PendingSend.Add(message);
         }
 
         private static ProxyServer CreateZoneProxy(string ip, int port)
         {
-            if (ip == string.Empty || port == 0) return null;
-            int proxyPort = ForwardingConnection.s_random.Next(23000, 44000);
-            var proxyAddr = new IPEndPoint(Program.s_proxyIP, proxyPort);
+            if (string.IsNullOrEmpty(ip) || port == 0) return null;
+            int proxyPort = ForwardingConnection.Random.Next(23000, 44000);
+            var proxyAddr = new IPEndPoint(Program.ProxyIP, proxyPort);
             var proxy = Program.AddProxyServer(proxyAddr, ip, port);
             return proxy;
         }
     }
-    
+
     public class ForwardingConnectionOwner : ForwardingConnection
     {
         public ForwardingConnectionOwner(Socket socket) : base(socket)
         {
             var localPort = ((IPEndPoint)socket.LocalEndPoint).Port;
-            var remotePort = ((IPEndPoint)socket.RemoteEndPoint).Port;
             m_proxy = Program.GetProxyServer(localPort);
 
             var id = Interlocked.Increment(ref s_currConnectionID);
-            
+
             m_dump = new PacketDump($"packets_from_client_{id}.bin");
             var dumpOther = new PacketDump($"packets_from_server_{id}.bin");
 
             var remoteSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            remoteSocket.Connect(m_proxy.m_externalEndpoint);
-            m_peer = new ForwardingConnection(remoteSocket) {m_peer = this, m_dump = dumpOther, m_proxy = m_proxy};
+            remoteSocket.Connect(m_proxy.ExternalEndpoint);
+            m_peer = new ForwardingConnection(remoteSocket) { PeerConnection = this, Dump = dumpOther, Proxy = m_proxy };
             m_peer.Start();
             m_peer.StartListening();
         }
@@ -157,48 +152,35 @@ namespace Open101.Tools.PacketProxy
             base.Update();
         }
     }
-    
-    
-    public class PacketDump // todo: this implementation is so bad lol
+
+    public class PacketDump : IDisposable
     {
         private readonly string m_file;
         private Stream m_stream;
         private BinaryWriter m_writer;
-        
+
         public PacketDump(string file)
         {
-            /*m_file = file;
+            m_file = file;
             m_stream = File.OpenWrite(file);
             m_stream.SetLength(0);
-            m_writer = new BinaryWriter(m_stream);*/
+            m_writer = new BinaryWriter(m_stream);
         }
 
         public void Dispose()
         {
-            /*if (m_stream != null)
-            {
-                long length = m_stream.Length;
-                
-                m_stream.Flush();
-                m_stream.Dispose();
-                m_stream = null;
-                
-                if (length == 0) File.Delete(m_file);
-            }
-            if (m_writer != null)
-            {
-                m_writer.Dispose();
-                m_writer = null;
-            }
-            GC.SuppressFinalize(this);*/
+            m_writer?.Dispose();
+            m_stream?.Dispose();
+            m_writer = null;
+            m_stream = null;
+            GC.SuppressFinalize(this);
         }
 
         public void Write(byte[] data, int size)
         {
-            /*m_writer.Write(size);
+            m_writer.Write(size);
             m_writer.Write(data, 0, size);
-            
-            m_stream.Flush();*/
+            m_stream.Flush();
         }
 
         ~PacketDump()
